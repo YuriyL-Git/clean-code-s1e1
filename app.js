84e496dbd4b04415dfa3f46a01e81b0bc3f37476
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-27 11:55:35 +0200

    refactor: replace tag selectors according to rule 3.4

--- app.js
+++ app.js
@@ -33,22 +33,25 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement("img");//delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = "task";
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
+  checkBox.className = "checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.className = "input task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "btn edit";
 
-  deleteButton.className = "delete";
-  deleteButtonImg.src = './remove.svg';
+  deleteButton.className = "btn delete";
+  deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.className = "img";
   deleteButton.appendChild(deleteButtonImg);
 
 
   //and appending.
+  listItem.className = "list-item";
   listItem.appendChild(checkBox);
   listItem.appendChild(label);
   listItem.appendChild(editInput);
@@ -81,7 +84,7 @@ var editTask = function () {
 
   var listItem = this.parentNode;
 
-  var editInput = listItem.querySelector('input[type=text]');
+  var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
   var containsClass = listItem.classList.contains("edit-mode");
